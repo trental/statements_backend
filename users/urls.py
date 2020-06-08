ab3cbadfa9ec6ee2dd729d871cd761cf8632d381
trental <trentlehmann@gmail.com>
2020-06-08 15:21:43 -0500

    Inserted leading /

--- users/urls.py
+++ users/urls.py
@@ -3,6 +3,6 @@ from . import views
 from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
-    path('new', views.NewUser.as_view(), name='new_user'),
+    path('/new', views.NewUser.as_view(), name='new_user'),
     path('', views.UserData.as_view(), name='user'),
 ]
