2f9c6ba3026ccacc3df2d8d17dccf08117db3f75
trental <trentlehmann@gmail.com>
2020-06-09 09:35:54 -0500

    Added more transaction types to seed

--- seedDB/seed_transactions.sql
+++ seedDB/seed_transactions.sql
@@ -2,3 +2,27 @@ insert into statements_transaction (transaction_type, transaction_property, tran
 insert into statements_transaction (transaction_type, transaction_property, transaction_property_datatype) VALUES ('issue_common_stock', 'accounting_date', 'date');
 insert into statements_transaction (transaction_type, transaction_property, transaction_property_datatype) VALUES ('issue_common_stock', 'capital', 'number');
 insert into statements_transaction (transaction_type, transaction_property, transaction_property_datatype) VALUES ('issue_common_stock', 'description', 'text');
+insert into statements_transaction (transaction_type, transaction_property, transaction_property_datatype) VALUES ('borrow', 'transaction_date', 'date');
+insert into statements_transaction (transaction_type, transaction_property, transaction_property_datatype) VALUES ('borrow', 'accounting_date', 'date');
+insert into statements_transaction (transaction_type, transaction_property, transaction_property_datatype) VALUES ('borrow', 'loan_amount', 'number');
+insert into statements_transaction (transaction_type, transaction_property, transaction_property_datatype) VALUES ('borrow', 'current_debt', 'number');
+insert into statements_transaction (transaction_type, transaction_property, transaction_property_datatype) VALUES ('borrow', 'long_term_debt', 'number');
+insert into statements_transaction (transaction_type, transaction_property, transaction_property_datatype) VALUES ('borrow', 'description', 'text');
+insert into statements_transaction (transaction_type, transaction_property, transaction_property_datatype) VALUES ('purchase_ppe', 'transaction_date', 'date');
+insert into statements_transaction (transaction_type, transaction_property, transaction_property_datatype) VALUES ('purchase_ppe', 'accounting_date', 'date');
+insert into statements_transaction (transaction_type, transaction_property, transaction_property_datatype) VALUES ('purchase_ppe', 'cost_of_ppe', 'number');
+insert into statements_transaction (transaction_type, transaction_property, transaction_property_datatype) VALUES ('purchase_ppe', 'description', 'text');
+insert into statements_transaction (transaction_type, transaction_property, transaction_property_datatype) VALUES ('pay_accrued_expenses', 'transaction_date', 'date');
+insert into statements_transaction (transaction_type, transaction_property, transaction_property_datatype) VALUES ('pay_accrued_expenses', 'accounting_date', 'date');
+insert into statements_transaction (transaction_type, transaction_property, transaction_property_datatype) VALUES ('pay_accrued_expenses', 'accrued_expenses_paid', 'number');
+insert into statements_transaction (transaction_type, transaction_property, transaction_property_datatype) VALUES ('pay_accrued_expenses', 'description', 'text');
+insert into statements_transaction (transaction_type, transaction_property, transaction_property_datatype) 
+VALUES ('salary_and_employment_expenses', 'transaction_date', 'date');
+insert into statements_transaction (transaction_type, transaction_property, transaction_property_datatype) VALUES ('salary_and_employment_expenses', 'accounting_date', 'date');
+insert into statements_transaction (transaction_type, transaction_property, transaction_property_datatype) 
+VALUES ('salary_and_employment_expenses', 'salary_paid_general_and_administrative', 'number');
+insert into statements_transaction (transaction_type, transaction_property, transaction_property_datatype) 
+VALUES ('salary_and_employment_expenses', 'employment_expenses_accrued_general_and_administrative', 'number');
+insert into statements_transaction (transaction_type, transaction_property, transaction_property_datatype) VALUES ('salary_and_employment_expenses', 'salary_paid_sales_and_marketing', 'number');
+insert into statements_transaction (transaction_type, transaction_property, transaction_property_datatype) VALUES ('salary_and_employment_expenses', 'employment_expenses_accrued_sales_and_marketing', 'number');
+insert into statements_transaction (transaction_type, transaction_property, transaction_property_datatype) VALUES ('salary_and_employment_expenses', 'description', 'text');
